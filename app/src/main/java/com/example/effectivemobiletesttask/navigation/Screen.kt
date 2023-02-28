2e099e79e54d73e8d24fe6c293153644d92249f8
Anette11 <krivaltseva@gmail.com>
2023-02-28 13:35:51 +0400

    Create ShopBottomNavigation

--- app/src/main/java/com/example/effectivemobiletesttask/navigation/Screen.kt
+++ app/src/main/java/com/example/effectivemobiletesttask/navigation/Screen.kt
@@ -1,9 +1,44 @@
 package com.example.effectivemobiletesttask.navigation
 
+import androidx.annotation.DrawableRes
+import com.example.effectivemobiletesttask.R
+
 sealed class Screen(
-    val route: String
+    val route: String,
+    @DrawableRes val icon: Int?
 ) {
-    object LogIn : Screen(route = "log_in_screen")
-    object SingIn : Screen(route = "sign_in_screen")
-    object Profile : Screen(route = "profile_screen")
+    object LogIn : Screen(
+        route = "log_in_screen",
+        icon = null
+    )
+
+    object SingIn : Screen(
+        route = "sign_in_screen",
+        icon = null
+    )
+
+    object Home : Screen(
+        icon = R.drawable.ic_home,
+        route = "home_screen"
+    )
+
+    object Favourite : Screen(
+        icon = R.drawable.ic_favourite,
+        route = "favourite_screen"
+    )
+
+    object Shopping : Screen(
+        icon = R.drawable.ic_shopping,
+        route = "shopping_screen"
+    )
+
+    object Chat : Screen(
+        icon = R.drawable.ic_chat,
+        route = "chat_screen"
+    )
+
+    object Profile : Screen(
+        icon = R.drawable.ic_profile,
+        route = "profile_screen"
+    )
 }
---
+++ app/src/main/java/com/example/effectivemobiletesttask/ui/common/ShopBottomNavigation.kt
@@ -0,0 +1,92 @@
+package com.example.effectivemobiletesttask.ui.common
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.AppBarDefaults
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.effectivemobiletesttask.R
+import com.example.effectivemobiletesttask.navigation.Screen
+
+@Composable
+fun ShopBottomNavigation(
+    items: List<Screen>,
+    navController: NavHostController,
+    onNavigateClick: (String) -> Unit
+) = BottomNavigation(
+    modifier = Modifier
+        .fillMaxWidth()
+        .clip(
+            RoundedCornerShape(
+                topStart = dimensionResource(id = R.dimen._25dp),
+                topEnd = dimensionResource(id = R.dimen._25dp)
+            )
+        ),
+    backgroundColor = colorResource(id = R.color.white),
+    contentColor = colorResource(id = R.color.gray_extra),
+    elevation = AppBarDefaults.BottomAppBarElevation
+) {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+    items.forEach { item ->
+        val isSelected = currentRoute?.contains(item.route) ?: false
+        BottomNavigationItem(
+            modifier = Modifier.padding(dimensionResource(id = R.dimen._12dp)),
+            selected = isSelected,
+            onClick = { onNavigateClick(item.route) },
+            icon = {
+                Box(
+                    modifier = Modifier
+                        .height(35.dp)
+                        .width(35.dp)
+                        .clip(CircleShape)
+                        .background(
+                            color = if (isSelected) colorResource(id = R.color.gray_lighter)
+                            else Color.Transparent
+                        ),
+                    contentAlignment = Alignment.Center,
+                ) {
+                    Icon(
+                        painter = painterResource(id = item.icon ?: R.drawable.ic_placeholder),
+                        contentDescription = null
+                    )
+                }
+            },
+            alwaysShowLabel = false,
+            selectedContentColor = colorResource(id = R.color.gray_blue),
+            unselectedContentColor = colorResource(id = R.color.gray_extra)
+        )
+    }
+}
+
+@Composable
+@Preview
+fun ShopBottomNavigationPreview() =
+    ShopBottomNavigation(
+        items = listOf(
+            Screen.Home,
+            Screen.Favourite,
+            Screen.Shopping,
+            Screen.Chat,
+            Screen.Profile
+        ),
+        navController = rememberNavController(),
+        onNavigateClick = {}
+    )
