5ef25301f462dce9b0606d7cd065b951c64e8548
Anette11 <krivaltseva@gmail.com>
2023-03-06 23:52:18 +0400

    Create data class GetGoodsDetailsResponse

---
+++ data/src/main/java/com/example/data/remote/goods_details/GetGoodsDetailsResponse.kt
@@ -0,0 +1,19 @@
+package com.example.data.remote.goods_details
+
+import com.google.gson.annotations.SerializedName
+
+data class GetGoodsDetailsResponse(
+    val colors: List<String> = emptyList(),
+    val description: String?,
+
+    @SerializedName(value = "image_urls")
+    val imageUrls: List<String> = emptyList(),
+
+    val name: String?,
+
+    @SerializedName(value = "number_of_reviews")
+    val numberOfReviews: Int?,
+
+    val price: Int?,
+    val rating: Double?
+)
